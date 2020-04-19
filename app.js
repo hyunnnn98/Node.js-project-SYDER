1e90d34ca42acc744a420793be5b440f9fe32761
kokomade98 <c0305sh@naver.com>
2020-04-19 20:22:20 +0900

    HTTP Request TEST

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express   = require('express');
+const cors      = require('cors');
 const path      = require('path');
 const flash     = require('connect-flash');
 require('dotenv').config()
@@ -11,11 +12,15 @@ const app = express();
 // mongoConnect();      // AWS 몽고DB 연결 (X).
 
 // CORS 모든 도메인 권한 풀기.
-app.all('/*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
+// app.all('/*', function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     next();
+// });
+app.use(cors())
+app.get('/products/:id', function (req, res, next) {
+    res.json({ msg: 'This is CORS-enabled for all origins!' })
+})
 
 // pug로 웹페이지 통신.
 app.set('views', path.join(__dirname, 'views'));
@@ -29,6 +34,12 @@ app.use(flash());
 
 // 초기 페이지 설정
 app.use('/', indexRouter);
+app.use('/status', indexRouter);
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 const server = app.listen(app.get('port'), () => {
     console.log('Listening at port number :', app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
-
+const axios = require('axios');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    // res.render('index');
 });
+
+router.post('/status', async (req, res) => {
+    try {
+        console.log("다른 루트로부터 연결 들어왔음!!", req.body.message)
+        await axios.get('http://bokvengers.iptime.org/api/node')
+            .then(res => {
+                console.log("데이터 받았어!");
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
+        return res.json({
+            code: 200,
+            message: '노드 서버 연결 성공 => ' + req.body.message
+        });
+    } catch (err) {
+        console.log("서버 측 오류!", err);
+        return res.status(500).json({
+            code: 500,
+            message: '서버 오류입니다.'
+        })
+    };
+
+
+})
 
 module.exports = router;
